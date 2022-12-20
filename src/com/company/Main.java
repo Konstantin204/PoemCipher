f26aa0f96ae2deccb3fbe7ef53dcf4e4e21c89b2
Konstantin Georgiev <92094692+Konstantin204@users.noreply.github.com>
2022-12-20 13:10:38 +0200

    final program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
-
-public class Main {
-        public static void main(String[] args) throws IOException {
-            List<String> data = new ArrayList<String>();
-            Scanner userInput = new Scanner(System.in);
-            String message = userInput.nextLine();
-            int k = message.length();
-            Map<String, Character> helperMap = new HashMap<>();
-            String numberFirst;
-            String numberSecond;
-            List<String> crypted = new ArrayList<>();
-            List<String> letters = new ArrayList<>();
-            Random rand = new Random();
-
-
+public class Main{
+        public static List<String> returnData() throws IOException {
+            List<String> data = new ArrayList<>();
             BufferedReader bf = new BufferedReader(
                     new FileReader("lotr.txt"));
             String line = bf.readLine();
@@ -29,30 +17,83 @@ public class Main {
             }
             bf.close();
 
-            for(int i = 0; i < data.size(); i++){
-                for(int j = 0; j < data.get(i).length(); j++){
-                    numberFirst = Integer.toString(i);
-                    numberSecond = Integer.toString(j);
-                    helperMap.put(numberFirst+" "+numberSecond, data.get(i).charAt(j));
-                }
-            }
+            return data;
+        }
+        public static List<String> encrypting(String message) throws IOException{
 
-            for(int i = 0; i < message.length(); i++){
-                for(Map.Entry<String, Character> entry: helperMap.entrySet()) {
-                    if(entry.getValue() == message.charAt(i)) {
+            List<String> data = returnData();
+            HashMap<String, Character> helperMap = new HashMap<>();
+            List<String> crypted = new ArrayList<>();
+            List<String> letters = new ArrayList<>();
+            Random rand = new Random();
+
+
+            textToMap(data, helperMap);
+
+            for (int i = 0; i < message.length(); i++) {
+                for (Map.Entry<String, Character> entry : helperMap.entrySet()) {
+                    if (entry.getValue() == message.charAt(i)) {
                         crypted.add(entry.getKey());
                     }
                 }
                 letters.add(crypted.get(rand.nextInt(crypted.size())));
                 crypted.clear();
             }
-            for (String element:letters
-                 ) {
-                System.out.println(element);
+
+            return letters;
+        }
+        public static String decrypting(List<String> letters) throws IOException{
+
+            List<String> data = returnData();
+            HashMap<String, Character> helperMap = new HashMap<>();
+            String numberFirst;
+            String numberSecond;
+            String message = "";
+
+
+            textToMap(data, helperMap);
+
+            for(int i = 0; i < letters.size(); i++){
+                message+=helperMap.get(letters.get(i)).toString();
+            }
+
+            return message;
+    }
+
+    public static HashMap<String, Character> textToMap(List<String> data, HashMap<String, Character> helperMap) {
+        String numberFirst;
+        String numberSecond;
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = 0; j < data.get(i).length(); j++) {
+                numberFirst = Integer.toString(i+1);
+                numberSecond = Integer.toString(j+1);
+                helperMap.put(numberFirst + " " + numberSecond, data.get(i).charAt(j));
+            }
+        }
+        return helperMap;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+            String decoded;
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Enter text for Poem Code:");
+            String message = userInput.nextLine();
+            List<String> letters = encrypting(message);
+            System.out.println("Choose option 1/2 -> encrypting/decrypting");
+            Scanner userChoice = new Scanner(System.in);
+            String userCh = userChoice.nextLine();
+
+            if(userCh.equals("1")){
+                System.out.println(letters);
+            }
+            if (userCh.equals("2")){
+                if(letters.isEmpty()) System.out.println("No message for encryption provided");
+                else{
+                    decoded = decrypting(letters);
+                    System.out.println(decoded);
+                }
             }
 
         }
-
-
-
 }
